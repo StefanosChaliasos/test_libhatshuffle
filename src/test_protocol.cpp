b2d9af4cbf72c70f69e1e36da2da245f3c6d1c47
Stefanos Chaliasos <t8140192@dias.aueb.gr>
2018-05-04 13:20:50 +0300

    Add protocol test

--- src/test_protocol.cpp
+++ src/test_protocol.cpp
@@ -20,20 +20,7 @@ int main(int argc, char *argv[])
     using milli = std::chrono::milliseconds;
     auto start = std::chrono::high_resolution_clock::now();
 
-    string crs_file = "crs.json";
-    string votes_file = "votes.json";
-    string ciphertexts_file = "ciphertexts.json";
-    string proofs_file = "proofs.json";
-    string decrypted_votes_file = "decrypted_votes.json";
-
-    bool verification;
-
-    cout << create_crs(n, crs_file);
-    cout << generate_encoded_votes(crs_file, votes_file);
-    cout << encrypt(crs_file, votes_file, ciphertexts_file);
-    cout << prove(crs_file, ciphertexts_file, proofs_file);
-    verification = verify(crs_file, ciphertexts_file, proofs_file);
-    cout << decrypt(crs_file, votes_file, proofs_file, decrypted_votes_file);
+    cout << test_mixnet(n);
 
     auto finish = std::chrono::high_resolution_clock::now();
     cout << std::chrono::duration_cast<milli>(finish - start).count() << endl;
