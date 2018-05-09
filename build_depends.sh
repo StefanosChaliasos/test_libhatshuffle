09057839cfa1debdc188c6673166cc3e3d78570c
Stefanos Chaliasos <t8140192@dias.aueb.gr>
2018-05-09 04:07:34 +0300

    Updata build script

--- build_depends.sh
+++ build_depends.sh
@@ -1,5 +1,5 @@
+git submodule update --init
 cd depends/libhatshuffle
-git fetch && git checkout lib
 git submodule update --init --recursive
 cp libff_changes/depends/libff/libff/CMakeLists.txt depends/libff/libff/CMakeLists.txt
 cp libff_changes/depends/libff/depends/CMakeLists.txt depends/libff/depends/CMakeLists.txt
